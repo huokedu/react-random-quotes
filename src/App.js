eb32e941e25d20fc422931a587cc76053c1b50ee
Kev Comedia <kevcomedia@gmail.com>
2017-11-29 21:55:42 +0800

    Change selected quote index on button click

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import Quote from './Quote';
 import Controls from './Controls';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {selectedQuoteIndex: -1};
+    this.handleNewQuoteClick = this.handleNewQuoteClick.bind(this);
+  }
+
+  handleNewQuoteClick() {
+    this.setState((prevState, props) => ({
+      selectedQuoteIndex: ~~(Math.random() * props.quotes.length)
+    }));
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +23,7 @@ class App extends Component {
           <h1 className="App-title">Random Quotes</h1>
         </header>
         <Quote quote="An inspiring quote" />
-        <Controls />
+        <Controls onNewQuoteClick={this.handleNewQuoteClick} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import quotes from './quotes';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App quotes={quotes} />, document.getElementById('root'));
 registerServiceWorker();
